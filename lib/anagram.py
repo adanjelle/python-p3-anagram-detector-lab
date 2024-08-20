47d88858b872f6de80ab60cefb112b1b1a5c0653
adanjelle <adanjelle4@gmail.com>
2024-08-20 21:02:19 +0300

    complete

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,13 @@
-# your code goes here!
+# your cod
+class Anagram:
+    def __init__(self, word):
+        self.word = word.lower()
+
+    def match(self, possible_anagrams):
+        # Sort the characters of the word to create a standard form
+        sorted_word = sorted(self.word)
+        
+        # Return the list of anagrams
+        return [anagram for anagram in possible_anagrams if sorted(anagram.lower()) == sorted_word]
+
+
